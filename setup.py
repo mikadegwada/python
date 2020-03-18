0d2eee15e6ece90686c4feb8e3f93c81d4f2c4bd
mikadegwada <mvishaupt@gmail.com>
2020-03-17 20:04:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="mikaPy-mikadegwada", # Replace with your own username
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/mikadegwada/python",
     packages=setuptools.find_packages(),
     classifiers=[
